1dfc1c9433160b5a1b5156e39a172a0e0f691985
Gerhard Reitmayr <reitmayr@icg.tugraz.at>
2012-04-02 18:24:45 +0200

    fix up for CMake to find TooN if its not in third party

---
+++ cmake/FindTooN.cmake
@@ -0,0 +1,16 @@
+find_path(TOON_INCLUDE_PATH TooN/TooN.h
+	~/usr/include
+	~/usr/local/include
+	/usr/include
+	/usr/local/include
+	thirdparty
+)
+
+if(TOON_INCLUDE_PATH)
+	set(TOON_FOUND TRUE)
+	set(TOON_INCLUDE_PATHS ${TOON_INCLUDE_PATH} CACHE STRING "The include paths needed to use TooN")
+endif()
+
+mark_as_advanced(
+    TOON_INCLUDE_PATHS
+)
